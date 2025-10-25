4402685ff4ee4cce1f03777f67e0d77d66b94f58
모뜩 <moddkhw@gmail.com>
2025-10-26 07:49:54 +0900

    Docs: add comments for better code readability

--- src/main/java/com/everybite/Application.java
+++ src/main/java/com/everybite/Application.java
@@ -1,13 +1,17 @@
-package com.example.meal_tracker;
+package com.everybite; // 최상위 패키지
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Spring Boot 실행 및 자동 설정 관현 핵심 클래스를 import함.
 
-@SpringBootApplication
-public class MealTrackerApplication {
+@SpringBootApplication // 자동 설정 + 컴포넌트 스캔
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MealTrackerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // 프로젝트 실행 트리거
+		// 스프링 부트 애플리케이션을 실행시킴.
+		// 내부적으로 Spring 컨테이너를 생성하고 Bean 등록, 웹 서버(Tomcat) 시작 등 전체 초기화.
+    }
 }
+
+// Main Class.
