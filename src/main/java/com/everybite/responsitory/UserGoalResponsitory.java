1660af73becb55af8d1d3bc8dfc6235b82766aab
모뜩 <moddkhw@gmail.com>
2025-10-26 08:48:30 +0900

    Docs: add comments for better code readability

--- src/main/java/com/everybite/responsitory/UserGoalResponsitory.java
+++ src/main/java/com/everybite/responsitory/UserGoalResponsitory.java
@@ -1,10 +1,12 @@
 package com.everybite.repository;
 
-import com.everybite.entity.UserGoal;
+import com.everybite.entity.UserGoal; // 사용자 목표 칼로리 관련 엔티티 import.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserGoalRepository extends JpaRepository<UserGoal, Long> {
+
+    //사용자 ID로 해당 사용자의 목표 칼로리 조회.
     UserGoal findByUserId(Long userId);
 }
