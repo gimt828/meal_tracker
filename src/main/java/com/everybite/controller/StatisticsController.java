18875ba1fa28dd72101015581968ca17667b9c6b
모뜩 <moddkhw@gmail.com>
2025-10-26 08:31:08 +0900

    Docs: add comments for better code readability

--- src/main/java/com/everybite/controller/StatisticsController.java
+++ src/main/java/com/everybite/controller/StatisticsController.java
@@ -3,14 +3,16 @@ package com.everybite.controller;
 import com.everybite.dto.DailyStatisticsDto;
 import com.everybite.dto.MonthlyStatisticsDto;
 import com.everybite.service.StatisticsService;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
-@RestController
-@RequestMapping("/api/statistics")
+@RestController // JSON 형태로 응답을 반환하는 REST API 컨트롤러임을 명시.
+@RequestMapping("/api/statistics") // 해당 컨트롤러의 기본 URI 경로 지정.
+    // 이 클래스의 모든 요청 경로 앞에는 "/api/statistics"가 붙음.
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
@@ -26,10 +28,10 @@ public class StatisticsController {
      */
     @GetMapping("/daily")
     public ResponseEntity<DailyStatisticsDto> getDailyStatistics(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-            @RequestParam Long userId
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, // 쿼리스트랑(?date=...)을 LocalDate 타입으로 자동 변환.
+            @RequestParam Long userId // {?userID=...) 파라미터를 Long 타입으로 받음.
     ) {
-        DailyStatisticsDto stats = statisticsService.getDailyStatistics(date, userId);
+        DailyStatisticsDto stats = statisticsService.getDailyStatistics(date, userId); // 하루 통계 데이터 조회.
         return ResponseEntity.ok(stats);
     }
 
