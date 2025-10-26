772b5688edb4844135e46ddd85929b6bc7c27bed
모뜩 <moddkhw@gmail.com>
2025-10-26 20:05:05 +0900

    Docs: add comments for better code readability

--- src/main/java/com/everybite/dto/MealCaloriesDto.java
+++ src/main/java/com/everybite/dto/MealCaloriesDto.java
@@ -3,8 +3,8 @@ package com.everybite.dto;
 import java.util.List;
 
 public class MealCaloriesDto {
-    private double totalGoal; // 하루 권장 칼로리
-    private List<ChartDataDto> meals; // label, value 구조
+    private double totalGoal; // 하루 권장(목표) 칼로리
+    private List<ChartDataDto> meals; // 각 식사별 칼로리 데이터 (라벨 + 값 구조)
 
     public MealCaloriesDto() {}
 
