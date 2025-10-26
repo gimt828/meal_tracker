045d61f5a60f7546c0871d38529a7f46063587b3
모뜩 <moddkhw@gmail.com>
2025-10-26 09:06:05 +0900

    Add: @Quary

--- src/main/java/com/everybite/responsitory/MealIntakeRecordResponsitory.java
+++ src/main/java/com/everybite/responsitory/MealIntakeRecordResponsitory.java
@@ -16,4 +16,14 @@ public interface MealIntakeRecordRepository extends JpaRepository<MealIntakeReco
     // 날짜 범위 내의 섭취 기록 조회 (start ~ end)
     List<MealIntakeRecord> findByDateBetween(LocalDate start, LocalDate end);
 
+    //그래프 1의 탄단지 합계 계산용
+    @Query("""
+            SELECT 
+                SUM(m.carbohydrates), 
+                SUM(m.protein), 
+                SUM(m.fat)
+            FROM MealIntakeRecord m
+            WHERE m.date = :date
+        """)
+        List<Object[]> findDailyMacroSums(@Param("date") LocalDate date);
 }
