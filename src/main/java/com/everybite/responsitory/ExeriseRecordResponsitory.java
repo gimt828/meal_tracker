b46bedd65232923b8173b8244bb74644df89a9b2
모뜩 <moddkhw@gmail.com>
2025-11-03 03:23:19 +0900

    Update ExeriseRecordResponsitory.java

--- src/main/java/com/everybite/responsitory/ExeriseRecordResponsitory.java
+++ src/main/java/com/everybite/responsitory/ExeriseRecordResponsitory.java
@@ -2,21 +2,20 @@ package com.everybite.repository;
 
 import com.everybite.entity.ExerciseRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-    // 스프링에게 이 인터페이스가 데이터 접근 계층임을 명시.
-    // 런타임 시 JPA가 이 인터페이스의 구현체를 자동 생성함.
-    
 public interface ExerciseRecordRepository extends JpaRepository<ExerciseRecord, Long> {
-    // JpaRepository<엔티티, PK타입>을 상속받아 기본 CRUD 메서드 제공
-
-    //특정 날짜의 운동 기록 조회
     List<ExerciseRecord> findByDate(LocalDate date);
-    // 날짜 범위 내의 운동 기록 조회 (start ~ end)
     List<ExerciseRecord> findByDateBetween(LocalDate start, LocalDate end);
+    
+    // 오늘 운동 소모 칼로리 합계
+    @Query("SELECT COALESCE(SUM(e.calories_burned), 0) FROM ExerciseRecord e WHERE e.date = :today")
+    double sumCaloriesBurnedForToday(@Param("today") LocalDate today);
 
 }
